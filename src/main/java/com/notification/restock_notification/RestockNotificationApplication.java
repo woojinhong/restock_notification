7254826c402559bdca3e4b756b38bdf660ccd1f9
woojin <orolzleim@gmail.com>
2024-10-12 10:29:32 +0900

    first init

---
+++ src/main/java/com/notification/restock_notification/RestockNotificationApplication.java
@@ -0,0 +1,13 @@
+package com.notification.restock_notification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestockNotificationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestockNotificationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/notification/restock_notification/RestockNotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.notification.restock_notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestockNotificationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
